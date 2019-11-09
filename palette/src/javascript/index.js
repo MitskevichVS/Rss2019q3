398cf6f3fa7e01eb3b90d7e7ed8602501dcb0479
Mitskevich Victor <mv2.mx@yandex.ru>
2019-11-09 16:08:13 +0300

    feat: add paint bucket tool

--- palette/src/javascript/index.js
+++ palette/src/javascript/index.js
@@ -4,6 +4,7 @@ import Canvas from './canvas/Canvas.js';
 class App {
   constructor() {
     this.app = {
+      canvasSize: 32,
       primaryColor: '#010101',
       secondaryColor: '#ffffff',
       currentTool: '',
@@ -15,13 +16,9 @@ class App {
     const canvas = new Canvas();
     canvas.setResolution();
 
-    console.log(canvas);
 
     function addListeners() {
       const toolsConatainer = document.querySelector('.main-container__tools__pallete');
-      // const colorContainer = document.querySelector('.main-container__tools__colors');
-      // const canvas = document.querySelector('#canvas');
-      // const ctx = canvas.getContext('2d');
       const canvasContainer = document.querySelector('.canvas_background');
 
       toolsConatainer.addEventListener('click', (event) => {
@@ -44,6 +41,9 @@ class App {
           case 'Bucket':
             canvas.removeEventListenersCanvas();
             canvas.paintBucket(this);
+            break;
+          case 'Color':
+            canvas.removeEventListenersCanvas();
             break;
           default:
             break;
