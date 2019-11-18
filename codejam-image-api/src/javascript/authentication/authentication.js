ff923d2c1c5ad1487e26f36442782055a5f77da0
Mitskevich Victor <mv2.mx@yandex.ru>
2019-11-18 16:12:18 +0300

    fix: show user after authentification

--- codejam-image-api/src/javascript/authentication/authentication.js
+++ codejam-image-api/src/javascript/authentication/authentication.js
@@ -2,6 +2,10 @@ import netlify from 'netlify-auth-providers';
 
 const authentication = () => {
   const errorMessage = document.querySelector('.header__container-error');
+  const userLogo = document.querySelector('.user__image');
+  const userNickname = document.querySelector('.user__name');
+  const loginButton = document.querySelector('.header__button-oauth');
+  const userContainer = document.querySelector('.header__container-user');
   const authenticator = new netlify({
     site_id: '505fe7e3-3366-48e1-99eb-3f34263e84de',
   });
@@ -10,22 +14,22 @@ const authentication = () => {
     if (err) {
       errorMessage.classList.remove('header__container-error-hide');
     } else {
-      fetch('https://api.github.com/user',
+      const targetUrl = 'https://api.github.com/user';
+      fetch(targetUrl,
         {
           method: 'GET',
-          withCredentials: true,
-          credentials: 'include',
           headers: {
-            'Access-Control-Allow-Origin': 'https://codejam-api-mvs.netlify.com/',
-            Authorization: `token ${data.token}`,
-            'Access-Control-Expose-Headers': '*',
+            Authorization: ` token ${data.token}`,
           },
         })
-        .then((responce) => {
-          console.log(responce);
-          responce.json();
-        })
-        .then(userData => console.log(userData));
+        .then(responce => responce.json())
+        .then((userData) => {
+          errorMessage.classList.add('header__container-error-hide');
+          loginButton.classList.add('header__button-oauth-hide');
+          userLogo.src = userData.avatar_url;
+          userNickname.childNodes[3].textContent = userData.login;
+          userContainer.classList.add('header__container-user-show');
+        });
     }
   });
 };
